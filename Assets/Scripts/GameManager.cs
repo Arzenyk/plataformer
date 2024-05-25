2f813d43400bf044d047bd2dd6619e6b75f1711e
Arzenyk <guillermitopereda@gmail.com>
2024-05-25 11:27:56 -0300

    Nueva scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -44,6 +45,7 @@ public class GameManager : MonoBehaviour
         if (i >= 7)
         {
             winCondition = true;
+            SceneManager.LoadScene(1);
         }
     }
 
